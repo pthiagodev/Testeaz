886a3df1da8a330b66b6497fe7b4a07c4a38f736
Thiago de Assis <106350838+pthiagodev@users.noreply.github.com>
2024-03-25 07:30:55 -0300

    Remove imports inutilizados

--- src/main/java/com/pthiago/leilaoapi/entity/Leilao.java
+++ src/main/java/com/pthiago/leilaoapi/entity/Leilao.java
@@ -6,18 +6,14 @@ import java.util.List;
 
 import org.hibernate.validator.constraints.Length;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
